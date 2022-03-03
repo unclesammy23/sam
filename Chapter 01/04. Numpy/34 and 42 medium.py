431f8ed1687dbb0d21db0783d9ed8a002458c240
samuel Hailu <samuelamarehailu99@gmail.com>
2022-03-03 15:21:43 +0100

    34 and 42

---
+++ Chapter 01/04. Numpy/34 and 42 medium.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+#34 working on it :(
+
+
+#42
+x= np.random.randint((3,3))
+y= np.random.randint((3,3))
+if y=x:
+    print("these arrays are equal")
+else:
+    print("they are not an equal") 
