b42acce590cc95a3dfbd786a2abff79f0e9faa7c
samuel Hailu <samuelamarehailu99@gmail.com>
2022-03-07 16:25:49 +0100

    ex 3

---
+++ excersice 3.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+data = {
+1:[np.nan,2,np.nan,4,5],
+2:[np.nan,2,3,np.nan,5],
+3:[np.nan,2,np.nan,4,5],
+4:[1,np.nan,3,np.nan,np.nan],
+5:[1,2,np.nan,4,5],
+6:[1,np.nan,3,np.nan,np.nan],
+7:[1,2,3,4,5],
+8:[1,np.nan,3,np.nan,np.nan],
+9:[1,2,3,4,np.nan],
+10:[1,np.nan,3,4,5]}
+
+
+df=pd.DataFrame(data)
+for i in range 10:
+  df[df.i==5]
+  print(i)
