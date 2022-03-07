862bc641abd556ea6a14ffcfc220ca509abccd29
unclesammy23 <100681493+unclesammy23@users.noreply.github.com>
2022-03-07 16:30:03 +0100

    Update excersice 3.py

--- excersice 3.py
+++ excersice 3.py
@@ -12,7 +12,7 @@ data = {
 8:[1,np.nan,3,np.nan,np.nan],
 9:[1,2,3,4,np.nan],
 10:[1,np.nan,3,4,5]}
-
+############################ sorry teacher I did not understood this one I uploaded it becasue you said so ############
 
 df=pd.DataFrame(data)
 for i in range 10:
